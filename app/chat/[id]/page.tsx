b068b005556b8ea5a14667bf1059957e65c62e74
Tito Ardimam Rahman <48154253+TitoRahman@users.noreply.github.com>
2024-12-11 01:15:23 +0700

    Messaging Container

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,7 +1,50 @@
 import React from "react";
-
+type Chat = {
+  role: "user" | "llama";
+  message: string;
+};
 function Page({ params }: { params: { id: string } }) {
-  return <div>{params.id}</div>;
+  let chat = generateRandomChat(20);
+  function generateRandomChat(n: number): Chat[] {
+    let chat: Chat[] = [];
+    for (let i = 0; i < n; i++) {
+      if (i % 2 === 0) {
+        chat.push({
+          role: "user",
+          message:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus.",
+        });
+      } else {
+        chat.push({
+          role: "llama",
+          message:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus.",
+        });
+      }
+    }
+    return chat;
+  }
+  return (
+    <div className={"w-full flex flex-col items-center"}>
+      <div className="w-1/2">
+        {chat.map((message, index) => (
+          <div
+            key={index}
+            className={`p-2 flex flex-col gap-2 ${message.role === "user" ? "items-end" : "items-start"}`}
+          >
+            <div className={"font-bold"}>
+              {message.role === "user" ? "" : "Llama"}
+            </div>
+            <div
+              className={`rounded w-3/4 ${message.role === "user" ? "bg-neutral-700 p-2" : ""}`}
+            >
+              {message.message}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Page;
--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -15,8 +15,14 @@ function ChatLayout({
   return (
     <div className={"flex"}>
       <Sidebar />
-      <div className="flex flex-col w-full h-screen p-8">
-        <div className={"flex-1"}>{children}</div>
+      <div className="flex flex-col w-full h-screen py-4">
+        <div
+          className={
+            "flex-1 overflow-scroll overflow-x-hidden [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500"
+          }
+        >
+          {children}
+        </div>
         <ChatTextInput onEnter={handleEnter} />
       </div>
     </div>
